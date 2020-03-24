dafd68a03b76974d4201a15dbb079f96a1f29825
april <nkzhangzhang@gmail.com>
2020-03-24 16:24:25 -0700

    update whitenoise storage

--- helloworld_project/settings.py
+++ helloworld_project/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # tell system where to find all static files
 LOGIN_REDIRECT_URL = 'posts'
 LOGOUT_REDIRECT_URL = 'posts'
